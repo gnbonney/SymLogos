0da14cd0d3ab2f6ff897beac8a2165f1ae413947
Greg Bonney <gnbonney@gmail.com>
2023-03-27 09:47:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="A higher-order modal logic extension for SymPy",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/symlogos",
+    url="https://github.com/gnbonney/symlogos",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
